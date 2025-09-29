9cc7e255ff9265b36e8ed40a72f60c93c92e0795
Thomas Silva de Farias <thomas.farias@luizalabs.com>
2025-09-29 10:08:36 -0300

    feat: validate candidate entity and error handler

--- src/main/java/br/com/thomasfarias/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/thomasfarias/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.thomasfarias.gestao_vagas.modules.candidate.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import br.com.thomasfarias.gestao_vagas.modules.candidate.CandidateEntity;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
     }
