0ebe171f02b3860cfe5151be702bb17a42561f16
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-09-29 21:25:45 -0300

    feat: create candidate in db

--- src/main/java/br/com/thomasfarias/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/thomasfarias/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,12 +1,18 @@
 package br.com.thomasfarias.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 @Data
@@ -14,10 +20,11 @@ import org.hibernate.validator.constraints.Length;
 public class CandidateEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
-    @NotBlank()
+    @NotBlank
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
 
@@ -28,4 +35,7 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime cratedAt;
 }
