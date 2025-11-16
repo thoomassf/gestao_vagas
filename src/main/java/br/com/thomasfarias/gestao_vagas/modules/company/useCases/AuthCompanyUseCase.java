9502d4926329e0d8db96d3f3b6cdc6914b9076a3
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-11-16 19:17:52 -0300

    feat: implementing get jwt and verifify token

--- src/main/java/br/com/thomasfarias/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/thomasfarias/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.thomasfarias.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,7 +44,9 @@ public class AuthCompanyUseCase {
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
     var token = JWT.create().withIssuer("javagas")
-        .withSubject(company.getId().toString()).sign(algorithm);
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
 
     return token;
   }
--- src/main/java/br/com/thomasfarias/gestao_vagas/security/SecuriryConfig.java
+++ src/main/java/br/com/thomasfarias/gestao_vagas/security/SecuriryConfig.java
@@ -1,30 +1,36 @@
 package br.com.thomasfarias.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecuriryConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
   
     @Bean
-      SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-          http.csrf(csrf -> csrf.disable())
-              .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/candidate/").permitAll()
-                    .requestMatchers("/company/").permitAll()
-                    .requestMatchers("/auth/company").permitAll();
-                auth.anyRequest().authenticated();
-              })
-          ;
-          return http.build();
-      }
+        SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/candidate/").permitAll()
+                        .requestMatchers("/company/").permitAll()
+                        .requestMatchers("/auth/company").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+            ;
+            return http.build();
+        }
 
-      @Bean
-      public PasswordEncoder passwordEncoder() {
-          return new BCryptPasswordEncoder();
-      }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
 }
